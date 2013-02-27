68497d6d938924cdfab0b7d4509634d3bab4e172
Stephen V. Williams <svwilliams@gmail.com>
2013-02-27 16:00:12 -0700

    new faculty process for data ingest

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#----------------------------------------------
+# Run Script for CU Boulder Data Ingests
+#
+#----------------------------------------------
+
+# Call Oracle Database - Pass Script File and Recieve Files
+
+
+
+
+# Run Comparison program based on last run
+
+
+
+
+
+# Based on Last run and Changes Run individual Script Files against Setup
+
+
+
+
+# Verify
+
+
+
+
+# Move to Production
