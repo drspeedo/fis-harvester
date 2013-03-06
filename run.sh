f3bc060392f0938d4bee21bafadae7964658686e
Stephen V. Williams <svwilliams@gmail.com>
2013-03-06 12:45:33 -0700

    * departments/* - new process to handle ingest of departments  * departments/* TODO: Fix BlankNode Renaming  * facultyCountries/* - new process to handle ingest of faculty geo focuses  * facultyRegions/* - new process to handle ingest of faculty geo focuses  * facultyPositions/* - new process to handle ingest of faculty positions  * facultyPositions/* TODO: Fix CSVFetch to allow for seq numbered nodes  * facultyResearchOverviews/* - new process to handle ingest of faculty research overviews  * facultySubjectAreas - new process to handle ingest of faculty Subject Areas  * run.sh - began first steps in ingest process  * run.sh TODO: add python scripts, determine ability to call bash run scripts, add dictionary of files to run scripts, add review run ingest in python scripts if file changed, ...

--- run.sh
+++ run.sh
@@ -5,9 +5,29 @@
 #
 #----------------------------------------------
 
+#Import Passwords for system processes
+source ../syspwd
+
+#Set Date, Default Folder, Data Folders, Backup File Prefix
+ROOT=~/Source
+DATE=$(date +"%Y%m%d")
+OLD=cub-old-data
+NEW=cub-new-data
+TARFILE=cub-update
+INSTITUTE=CUB
+
+
 # Call Oracle Database - Pass Script File and Recieve Files
+echo exit | /Library/Oracle/instantclient_11_2/sqlplus $OR_USER/$OR_PWD@FACDB @../oracle-scripts/vivo_extract_cub.sql
 
+# Move files into position for review from Laon
+rm -rf $ROOT/$OLD/*
+cp -R $ROOT/$NEW/ $ROOT/$OLD/
+rm -rf $ROOT/$NEW/*
+scp laon:/tmp/fis_* $ROOT/$NEW/
 
+# Create backup of output files
+tar -cvzf '$TARFILE'-'$DATE'.tar.gz $Root/$New/fis_*
 
 
 # Run Comparison program based on last run
